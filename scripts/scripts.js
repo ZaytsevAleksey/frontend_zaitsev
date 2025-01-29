adcf8a7b1ca8e0ed97c2b019aa7ff35f2a45a592
ZaytsevAleksey <wearytears@yandex.com>
2025-01-29 16:09:02 +0000

    ...

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,6 +6,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const registerModal = document.getElementById('registerModal');
     const closeLoginModal = loginModal.querySelector('.close');
     const closeRegisterModal = registerModal.querySelector('.close');
+    const JoinNowBtn = document.getElementById('Login');
+    const JoinNowModal = document.getElementById('SignUp');
+    const closeJoinNowModal = document.getElementById('loginModal');
 
 Login.addEventListener('click', function() {
     loginModal.style.display = "block";
@@ -13,11 +16,17 @@ Login.addEventListener('click', function() {
 SignUp.addEventListener('click', function() {
     registerModal.style.display = "block";
 });
- closeLoginModal.addEventListener('click', function() {
+JoinNow.addEventListener('cliick', function() {
+    JoinNowModal.style.display = "block";
+});
+closeLoginModal.addEventListener('click', function() {
     loginModal.style.display = "none";
 });
 closeRegisterModal.addEventListener('click', function() {
     registerModal.style.display = "none";
+});
+closeJoinNowModal.addEventListener('click', function() {
+    JoinNowModal.style.display = "none";
 });
 
 window.addEventListener('click', function(event) {
@@ -26,6 +35,9 @@ window.addEventListener('click', function(event) {
     }
     if (event.target == registerModal) {
         registerModal.style.display = "none";
+    }
+    if (event.target == JoinNowModal) {
+        JoinNowModal.style.display = "none";
     }
   })
 });

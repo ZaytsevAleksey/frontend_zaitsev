422a5c322ad884240729dc18aebc1904ece1d4a2
ZaytsevAleksey <wearytears@yandex.com>
2025-02-12 17:00:44 +0000

    ///

--- README.md
+++ README.md
@@ -3,37 +3,8 @@
 Макет: https://www.figma.com/design/u1fHPkVcFOPQjcQq3ZSnmr/Web.Jobly?node-id=0-1&node-type=canvas&t=51zzRpwNF1biqww6-0
 
 
-________________________________________________________________________________________________________
-Scripts.js
-const facilities = {
-    facilities_1:{
-        name: "",
-        icion: "",
-    },
-    facilities_1:{
-        name: "",
-        icion: "",
-    },
-    facilities_1:{
-        name: "",
-        icion: "",
-    },
-    facilities_1:{
-        name: "",
-        icion: "",
-    },
-    facilities_1:{
-        name: "",
-        icion: "",
-    },
-    facilities_1:{
-        name: "",
-        icion: "",
-    },
-    facilities_1:{
-        name: "",
-        icion: "",
-    },
-    
+.page__footer_footer {
+  background-color: #fff; 
+  color: #333; 
+  padding: 20px 0; 
 }
-______________________________________________________________________________________________________
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-
     const loginBtn = document.getElementById('Login');
     const registerBtn = document.getElementById('SignUp');
     const loginModal = document.getElementById('loginModal');
     const registerModal = document.getElementById('registerModal');
     const closeLoginModal = loginModal.querySelector('.close');
     const closeRegisterModal = registerModal.querySelector('.close');
-    const JoinNowBtn = document.getElementById('Login');
-    const JoinNowModal = document.getElementById('SignUp');
-    const closeJoinNowModal = document.getElementById('loginModal');
 
 Login.addEventListener('click', function() {
     loginModal.style.display = "block";
@@ -16,17 +12,11 @@ Login.addEventListener('click', function() {
 SignUp.addEventListener('click', function() {
     registerModal.style.display = "block";
 });
-JoinNow.addEventListener('cliick', function() {
-    JoinNowModal.style.display = "block";
-});
 closeLoginModal.addEventListener('click', function() {
     loginModal.style.display = "none";
 });
 closeRegisterModal.addEventListener('click', function() {
     registerModal.style.display = "none";
-});
-closeJoinNowModal.addEventListener('click', function() {
-    JoinNowModal.style.display = "none";
 });
 
 window.addEventListener('click', function(event) {
@@ -35,9 +25,6 @@ window.addEventListener('click', function(event) {
     }
     if (event.target == registerModal) {
         registerModal.style.display = "none";
-    }
-    if (event.target == JoinNowModal) {
-        JoinNowModal.style.display = "none";
     }
   })
 });
